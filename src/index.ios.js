2fc778fa0062acba17d3343efa0694a06eaf9571
YourtionGuo <yourtion@gmail.com>
2016-08-16 14:02:34 +0800

    move to index

--- src/index.ios.js
+++ src/index.ios.js
@@ -6,56 +6,14 @@
  */
 
 import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Text,
-  View,
-  ListView,
-} from 'react-native';
-import config from './config'
-import * as firebase from 'firebase';
-
-const StatusBar = require('./components/StatusBar');
-const ActionButton = require('./components/ActionButton');
-const ListItem = require('./components/ListItem');
-const styles = require('./styles.js');
-
-const firebaseApp = firebase.initializeApp(config.firebase);
+import {AppRegistry} from 'react-native';
+import Index from './index.js'
 
 class FirebaseRN extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSource: new ListView.DataSource({
-        rowHasChanged: (row1, row2) => row1 !== row2,
-      })
-    };
-  }
-
-  componentDidMount() {
-    this.setState({
-      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
-    })
-  }
-
-  _renderItem(item) {
-    return (
-      <ListItem item={item} />
-    );
-  }
-
   render() {
     return (
-      <View style={styles.container}>
-
-        <StatusBar title="Grocery List" />
-
-        <ListView dataSource={this.state.dataSource} renderRow={this._renderItem.bind(this)} style={styles.listview} />
-
-        <ActionButton title="Add" />
-
-      </View>
+      <Index />
     );
   }
 }
---
+++ src/index.js
@@ -0,0 +1,63 @@
+'use strict';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  ListView,
+} from 'react-native';
+
+import config from './config'
+import * as firebase from 'firebase';
+
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+const styles = require('./styles.js');
+
+const firebaseApp = firebase.initializeApp(config.firebase);
+
+class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
+    })
+  }
+
+  _renderItem(item) {
+    return (
+      <ListItem item={item} />
+    );
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView dataSource={this.state.dataSource} renderRow={this._renderItem.bind(this)} style={styles.listview} />
+
+        <ActionButton title="Add" />
+
+      </View>
+    );
+  }
+}
+
+module.exports = Index;
